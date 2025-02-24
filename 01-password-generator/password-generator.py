93b18227e88db8b35fc24b8ac71b7f7f81f5485d
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-02-24 14:59:28 -0400

    Agrego funcion generate_password

--- 01-password-generator/password-generator.py
+++ 01-password-generator/password-generator.py
@@ -1,13 +1,20 @@
 import secrets
 import string
 
-# Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+def generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
-# Combine all characters
-all_characters = letters + digits + symbols
+    # Combine all characters
+    all_characters = letters + digits + symbols
+    password = ""
+    # Generate password
+    for _ in range(length):
+        password += secrets.choice(all_characters)
 
-print(all_characters)
-print(secrets.choice(all_characters))
+    return password
+
+new_password = generate_password(8)
+print(new_password)
