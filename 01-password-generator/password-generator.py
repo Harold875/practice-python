bc7a17c24efc115e22c663209b86e33d07386405
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-02-26 10:00:59 -0400

    Agregando patrones a constraints

--- 01-password-generator/password-generator.py
+++ 01-password-generator/password-generator.py
@@ -16,12 +16,18 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         for _ in range(length):
             password += secrets.choice(all_characters)
 
-        constraints = [(nums, '[0-9]')]
+        constraints = [
+            (nums, r'\d'),
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),
+            (special_chars, r'\W')
+        ]
     return password
 
 # new_password = generate_password(8)
 # print(new_password)
 
-pattern = "[a-z]t"
+pattern = "[^a-z]"
+# pattern = r"\."
 quote = 'Not all those who wander are lost.'
 print(re.findall(pattern, quote))
