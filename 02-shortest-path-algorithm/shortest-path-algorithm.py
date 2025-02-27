5ba082caef89e934afc5d4a2690b49db175b0475
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-02-27 12:38:14 -0400

    bucle while casi terminado

--- 02-shortest-path-algorithm/shortest-path-algorithm.py
+++ 02-shortest-path-algorithm/shortest-path-algorithm.py
@@ -9,6 +9,18 @@ def shortest_path(graph, start):
     unvisited = list(graph)
     distances = {node:0 if node == start else float('inf') for node in graph }
     paths = {node:[] for node in graph}
-    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    paths[start].append(start)
+    while unvisited:
+        current = min(unvisited,key=distances.get)
+        for node, distance in graph[current]:
+            if distance + distances[current] < distances[node] :
+                distances[node] = distance + distances[current]
+                if paths[node][-1] == node:
+                    paths[node] = paths[current]
+                else:
+                    paths[node].extend(paths[current])
+                paths[node].append(node)
+
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
         
-shortest_path(my_graph, 'A')
+# shortest_path(my_graph, 'A')
