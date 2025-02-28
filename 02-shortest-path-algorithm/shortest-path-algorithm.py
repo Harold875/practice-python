8b65d9051eca5e8cce02df194c2956aa44ee5343
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-02-28 16:06:38 -0400

    Agregando Comentarios en la funcion shortest_path

--- 02-shortest-path-algorithm/shortest-path-algorithm.py
+++ 02-shortest-path-algorithm/shortest-path-algorithm.py
@@ -5,35 +5,39 @@ my_graph = {
     'D': [('A', 1), ('C', 7)]
 }
 
-my_graph = {
-    'A':  [('X', 4)],
-    'X': [('Y', 2), ('Z', 3), ('A', 4)],
-}
 # my_graph = {
-#     'A': [('B', 5), ('C', 3), ('E', 11)],
-#     'B': [('A', 5), ('C', 1), ('F', 2)],
-#     'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
-#     'D': [('C', 1), ('E', 9), ('F', 3)],
-#     'E': [('A', 11), ('C', 5), ('D', 9)],
-#     'F': [('B', 2), ('D', 3)]
+#     'A':  [('X', 4)],
+#     'X': [('Y', 2), ('Z', 3), ('A', 4)],
 # }
+my_graph = {
+    'A': [('B', 5), ('C', 3), ('E', 11)],
+    'B': [('A', 5), ('C', 1), ('F', 2)],
+    'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
+    'D': [('C', 1), ('E', 9), ('F', 3)],
+    'E': [('A', 11), ('C', 5), ('D', 9)],
+    'F': [('B', 2), ('D', 3)]
+}
 
 def shortest_path(graph, start, target=""):
     unvisited = list(graph)
     distances = {node:0 if node == start else float('inf') for node in graph }
     paths = {node:[] for node in graph}
     paths[start].append(start)
+    
     while unvisited:
         current = min(unvisited,key=distances.get)
+        # Handling disconnected nodes in the graph
         if distances[current] == float('inf'):
             paths[current].append("No hay ruta disponible")
             unvisited.remove(current)
             continue
+        
         for node, distance in graph[current]:
+            # checks for non-existing nodes
             if node not in distances:
                 distances[node] = float('inf')
                 paths[node] = []
-           
+
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
                 if paths[node] and paths[node][-1] == node:
@@ -45,6 +49,7 @@ def shortest_path(graph, start, target=""):
         unvisited.remove(current)
     targets_to_print = [target] if target else graph
     
+    # Print formatted output with distances and paths
     for node in targets_to_print:
         if node == start:
             continue
