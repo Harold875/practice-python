eb15b0870635281a4d4564a45429da0194a7a2a9
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-03-01 19:40:55 -0400

    empezando con la forma recursiva de tower-of-hanoi-puzzle

---
+++ 03-tower-of-hanoi-puzzle/tower_hanoi_recursive.py
@@ -0,0 +1,13 @@
+NUMBER_OF_DISKS = 4
+rods = {
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
+    "B": [],
+    "C": [],
+}
+
+def move(n, source, auxiliary, target):
+    # display starting configuration
+    print(rods,"\n")
+
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, "A", "B", "C")
