d2dd07b3715c9b67c96623aa4d50d803a08fedd5
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-03-01 10:55:29 -0400

    Agregando variable remainder

--- 03-tower-of-hanoi-puzzle/tower_hanoi_puzzle.py
+++ 03-tower-of-hanoi-puzzle/tower_hanoi_puzzle.py
@@ -6,6 +6,11 @@ rods = {
     "C": [],
 }
 def move(n, source, auxiliary, target):
-    print(rods)
-    
+    # display starting configuration
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if (i + 1) % 3 == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+
+# initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, "A", "B", "C")
