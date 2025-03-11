a22f7ab336d658b2158474349bb2df53c674b34f
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-03-11 16:00:04 -0400

    add method _delete() and _min_value() in BinarySearchTree

--- 06-binary-search-tree/binary_search_tree.py
+++ 06-binary-search-tree/binary_search_tree.py
@@ -38,6 +38,29 @@ class BinarySearchTree():
     def search(self, key):
         return self._search(self.root, key)
     
+    
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            node.key = self._min_value(node.right)
+            node.right = self._delete(node.right, node.key)
+        return node
+            
+    
+    def _min_value(self, node):
+        while node.left is not None:
+            node = node.left
+        return node.key
+    
 
 bst = BinarySearchTree()
 
@@ -46,4 +69,4 @@ nodes = [50, 30, 20, 40, 70, 60, 80]
 for node in nodes:
     bst.insert(node)
     
-print('Search for 80:', bst.search(80))
+# print('Search for 80:', bst.search(80))
