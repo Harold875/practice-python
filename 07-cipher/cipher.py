5be6531bdc55d16fbd62a58ca42812d9fd5dadcf
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-03-14 11:32:02 -0400

    start project 07-cipher

--- 07-cipher/cipher.py
+++ 07-cipher/cipher.py
@@ -1,6 +1,7 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0])
+index = alphabet.find(text[0].lower())
 print(index)
-print(text.lower())
+shifted = alphabet[index + shift]
+print(shifted)
