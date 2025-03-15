970a2ded69875bfff7e41258e737c25024d56676
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-03-15 16:16:52 -0400

    convirtiendo funcion caesar en funcion vigenere

--- 07-cipher/cipher.py
+++ 07-cipher/cipher.py
@@ -1,19 +1,25 @@
 text = 'Hello Zaira'
-shift = 3
+custom_key = 'python'
 
-def caesar(message, offset):
+def vigenere(message, key):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
     for char in message.lower():
+        # Append space to the message
         if char == ' ':
             encrypted_text += char
         else:
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]  
+            key_index += 1
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-    print('plain message:', message)
-    print('encrypted message:', encrypted_text)
+    return encrypted_text
     
-caesar(text, shift)
-caesar(text, 13)
+encryption = vigenere(text, custom_key)
+print(encryption)
