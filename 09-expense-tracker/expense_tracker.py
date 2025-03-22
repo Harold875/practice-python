ccd50a22adac894b82a4389ef632088c40268f2a
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-03-22 15:22:40 -0400

    add add_expense and print_expenses function

--- 09-expense-tracker/expense_tracker.py
+++ 09-expense-tracker/expense_tracker.py
@@ -1,18 +1,11 @@
-my_list = [1, 2]
+def add_expense(expenses, amount, category):
+    expenses.append({'amount':amount, 'category': category})
 
-# append() -> agrega el elemento al final
-my_list.append(3)
-print(my_list)
 
-print(my_list[0])
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+        
+def 
 
-my_list[0] = 0
-print(my_list)
-
-# insert() -> Agrega el elemento en un indice especifico
-my_list.insert(1, 1)
-print(my_list)
-
-# pop() -> elimina elemento, por defecto el ultimo, si se le pasa el indice eliminara el elemento del indice
-my_list.pop()
-print(my_list)
+expenses = []
