0fea395b9499e5fc4c093c6833846d58d3be855c
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-04-28 18:46:40 -0400

    modify __str__() and norm() methods in the R2Vector class

--- 12-vector-space/vector_space.py
+++ 12-vector-space/vector_space.py
@@ -5,21 +5,29 @@ class R2Vector:
         self.y = y
 
     def __str__(self):
-        return f'{self.x, self.y}'
+        return str(tuple(getattr(self,i) for i in vars(self)))
+
     
     
     def norm(self):
-        return (self.x**2 + self.y**2)**0.5
+        return (sum(val**2 for val in vars(self).values()))**0.5
+        
 
 
 class R3Vector(R2Vector):
+    
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
         
 
 v1 = R2Vector(x=2, y=3)
-print(v1.norm())
-print(v1)
 
 v2 = R3Vector(x=2, y=2, z=3)
+# print(v1.__dict__)
+# print(v1.norm())
+# print(v2.__dict__)
+# print(v2.norm())
+print(f'v1 = {v1}')
+print(f'v2 = {v2}')
+print(type(getattr(v1,'x')))
