7d78d164206fbfb3541ee600ab369234b7b3c270
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-04-28 20:31:57 -0400

    remove __getattr__() method from R2Vector class

--- 12-vector-space/vector_space.py
+++ 12-vector-space/vector_space.py
@@ -12,8 +12,6 @@ class R2Vector:
         args =  ', '.join(arg_list)
         return f'{self.__class__.__name__}({args})'
     
-    def __getattr__(self, attr):
-        return 'calling __getattr__'
     
     def norm(self):
         return (sum(val**2 for val in vars(self).values()))**0.5
@@ -31,8 +29,5 @@ v1 = R2Vector(x=2, y=3)
 
 v2 = R3Vector(x=2, y=2, z=3)
 
-# print(f'v1 = {v1}', f'\nrepr = {repr(v1)}')
-# print(f'v2 = {v2}', f'\nrepr = {repr(v2)}')
-print(v1.z)
-print(getattr(v1,'z'))
-print(v1.__getattribute__)
+print(f'v1 = {v1}')
+print(f'v2 = {v2}')
