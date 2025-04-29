a95ba03b74b203c61327ca286daeae4e5c6ef980
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-04-28 20:25:25 -0400

    replace __getattribute__ with __getattr__ in R2Vector class

--- 12-vector-space/vector_space.py
+++ 12-vector-space/vector_space.py
@@ -12,8 +12,8 @@ class R2Vector:
         args =  ', '.join(arg_list)
         return f'{self.__class__.__name__}({args})'
     
-    def __getattribute__(self, attr):
-        return 'calling __getattribute__'
+    def __getattr__(self, attr):
+        return 'calling __getattr__'
     
     def norm(self):
         return (sum(val**2 for val in vars(self).values()))**0.5
@@ -33,6 +33,6 @@ v2 = R3Vector(x=2, y=2, z=3)
 
 # print(f'v1 = {v1}', f'\nrepr = {repr(v1)}')
 # print(f'v2 = {v2}', f'\nrepr = {repr(v2)}')
-print(v1.x)
-print(getattr(v1,'x'))
+print(v1.z)
+print(getattr(v1,'z'))
 print(v1.__getattribute__)
