fb27b9f1329a73ac9cd80202b8db5602e40e17ca
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-05-05 19:43:14 -0400

    add abstract base structure to Equation class
    add __init__() and __init_subclass__() methods to Equation
    mark solve() and analyze() as @abstractmethod

--- 13-equation-solver/equation_solver.py
+++ 13-equation-solver/equation_solver.py
@@ -1,16 +1,42 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class Equation(ABC):
+    degree: int
+    
+    def __init__(self, *args):
+        if (self.degree + 1) != len(args):
+            raise TypeError(f"'{self.__class__.__name__}' object takes {self.degree + 1} positional arguments but {len(args)} were given")
+        
+        if any( not isinstance(arg,(int, float)) for arg in args):
+            raise TypeError("Coefficients must be of type 'int' or 'float'")
+        
+        if args[0] == 0:
+            raise ValueError('Highest degree coefficient must be different from zero')
+        
+        self.coefficients = { (self.degree - idx): arg for idx, arg in enumerate(args)}
+        
+          
+    def __init_subclass__(cls):
+        if not hasattr(cls,'degree'):
+            raise AttributeError(f"Cannot create '{cls.__name__}' class: missing required attribute 'degree'") 
+    
+    @abstractmethod
+    def solve(self):
+        pass
+    
+    @abstractmethod
+    def analyze(self):
+        pass
+    
+class LinearEquation(Equation):
+    degree = 1
     
     def solve(self):
         pass
     
     def analyze(self):
         pass
-    
-class LinearEquation(Equation):
-    pass
 
 
-eq = Equation()
-lin_eq = LinearEquation()
+
+lin_eq = LinearEquation(2, 3)
