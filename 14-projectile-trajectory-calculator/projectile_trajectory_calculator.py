0bf9121ebac2258a4a3429dac922d7bf9739f20b
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-05-10 18:51:21 -0400

    start proyect 14-projectile-trajectory-calculator

---
+++ 14-projectile-trajectory-calculator/projectile_trajectory_calculator.py
@@ -0,0 +1,13 @@
+import math
+
+GRAVITATIONAL_ACCELERATION = 9.81
+PROJECTILE = "∙"
+x_axis_tick = "T"
+y_axis_tick = "⊣"
+
+
+class Projectile:
+    def __init__(self,speed, height, angle ):
+        self.__speed = speed
+        self.__height = height
+        self.__angle = math.radians(angle)
