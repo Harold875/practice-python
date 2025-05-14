4d379f7ac79d94487bea6d9e643b3eeed4429943
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-05-14 09:55:05 -0400

    add Graph class

--- 14-projectile-trajectory-calculator/projectile_trajectory_calculator.py
+++ 14-projectile-trajectory-calculator/projectile_trajectory_calculator.py
@@ -9,7 +9,7 @@ y_axis_tick = "⊣"
 class Projectile:
     __slots__ = ('__speed', '__height', '__angle')
     
-    def __init__(self,speed, height, angle ):
+    def __init__(self, speed, height, angle ):
         self.__speed = speed
         self.__height = height
         self.__angle = math.radians(angle)
@@ -73,6 +73,46 @@ class Projectile:
     def angle(self, new_value):
         self.__angle = math.radians(new_value)
 
+
+class Graph:
+    __slots__ = ('__coordinates')
+    
+    def __init__(self, coord):
+        self.__coordinates = coord
+        
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.__coordinates})'
+
+    def create_coordinates_table(self):
+        table = f'\n{"x":>3}{"y":>7}\n'
+        for x, y in self.__coordinates:
+            table += f'{x:>3}{y:>7.2f}\n'
+        
+        return table
+
+    def create_trajectory(self):
+        rounded_coords = [(round(x), round(y)) for x, y in self.__coordinates]
+        
+        x_max, y_max = rounded_coords[0]
+        for x, y in rounded_coords:
+            x_max = x_max if x_max > x else x
+            y_max = y_max if y_max > y else y
+        
+        matrix_list = [[" " for _ in range(x_max +  1)] for _ in range(y_max + 1)]
+        
+        for x, y in rounded_coords:
+            matrix_list[-y-1][x] = PROJECTILE
+        
+        matrix = [''.join(row) for row in matrix_list]
+        
+        matrix_axes = [y_axis_tick + row for row in matrix]
+        matrix_axes.append(" " + x_axis_tick * (len(matrix[0])))
+
+        graph =  '\n'+ '\n'.join(matrix_axes) + '\n'
+
+        
+        return graph
+
 # test 
 ball = Projectile(10, 3, 45)
 print(ball)
@@ -80,3 +120,9 @@ coordinates = ball.calculate_all_coordinates()
 print(ball.speed)
 print(ball.angle)
 # displacement_of_ball = ball._Projectile__calculate_displacement() # 12.6173996009878
+graph = Graph(coordinates)
+# print(graph.create_coordinates_table())
+print(graph.create_trajectory())
+
+# for row in graph.create_trajectory():
+#     print(row)
